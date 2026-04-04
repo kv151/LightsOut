666181103bc27f728c72405ed0901cdf3d16ada4
kv151 <124073287+kv151@users.noreply.github.com>
2026-04-04 15:39:01 +0100

    implemented state machine for game logic

--- src/LightsOut.cpp
+++ src/LightsOut.cpp
@@ -1,21 +1,21 @@
 /* Lights Out - A 2 player reaction test game */
 #include <Arduino.h>
-#include<Wire.h>
+#include <Wire.h>
 
 #define DEBUG
 
 //Define global definitions here:
 /* ------------ SHIFT REGISTER -------------*/
-#define SRDATAPIN 4 //shift register serial pin
-#define SRCLKPIN 6 //serial clock pin
-#define SRLATCHPIN 7 //storage register clock pin (latch)
+#define SRDATAPIN 4         //shift register serial pin
+#define SRCLKPIN 6          //serial clock pin
+#define SRLATCHPIN 7        //storage register clock pin (latch)
 byte pattern = 0;
 /* ------------ GAME LEDS & BUTTON CONNECTIONS-----------------------*/
-#define P1BUTTONPIN 2 // p1's push button
-#define P2BUTTONPIN 3 // p2's push button
-#define P1LEDPIN 5 // pins for green win led
+#define P1BUTTONPIN 2       // p1's push button
+#define P2BUTTONPIN 3       // p2's push button
+#define P1LEDPIN 5          // pins for green win led
 #define P2LEDPIN 10 
-#define P1JUMPLEDPIN 8 //pins for yellow jumpstart leds
+#define P1JUMPLEDPIN 8      //pins for yellow jumpstart leds
 #define P2JUMPLEDPIN 9
 /*--------------GAME STATUS---------------*/
 volatile bool gameRunning = false;
@@ -24,12 +24,16 @@ volatile bool buttonPressedP2 = false;
 volatile bool jumpStartP1 = false;
 volatile bool jumpStartP2 = false;
 char winner = 0;
+
+enum gamestate {LINEUP, STARTSEQUENCE, LIGHTSOUT, GAMEOVER};
+gamestate currentState = LINEUP;
+
 /*------------------ Clocks and timers--------------------*/
 volatile unsigned int lightsOutClock = 0;
 volatile unsigned int reactionTimeP1 = 0;
 volatile unsigned int reactionTimeP2 = 0;
-#define STARTSEQUENCEDELAY 1000 //delay before start sequence initated
-int lightsOutDelay = random(200,3000); //random delay time for lights out between 0.2 and 3 seconds.
+#define STARTSEQUENCEDELAY 1000                 //delay before start sequence initated
+int lightsOutDelay = random(200,3000);          //random delay time for lights out between 0.2 and 3 seconds.
 
 //put function declarations here:
 void lightpattern();
@@ -37,7 +41,7 @@ void lightpattern();
 /*=========================MAIN ARDUINO CODE===========================================================*/
 void setup() {
     pinMode(P1BUTTONPIN, INPUT_PULLUP); // use an intenral pullup no resistor needed on the button
-    pinMode(P2BUTTONPIN,INPUT_PULLUP);
+    pinMode(P2BUTTONPIN, INPUT_PULLUP);
     #ifdef DEBUG
     Serial.begin(9600);
     #endif
@@ -52,84 +56,107 @@ void setup() {
 }
 
 void loop() {
-    //Start a new game
-    gameRunning = true;
-    if (winner != 0) {
-        lightsOutDelay = random(200, 3000);
-        buttonPressedP1 = false;
-        buttonPressedP2 = false;
-        jumpStartP1 = false;
-        jumpStartP2 = false;
-        digitalWrite(P1LEDPIN, LOW);
-        digitalWrite(P2LEDPIN, LOW);
-        digitalWrite(P1JUMPLEDPIN, LOW);
-        digitalWrite(P2JUMPLEDPIN, LOW);
-
-        // game loop
-        delay(STARTSEQUENCEDELAY); 
-        
-        lightpattern();
-        delay(lightsOutDelay);
-        // LIGHTS OUT!
-        pattern = 0;
-        digitalWrite(SRLATCHPIN, LOW);                    // prepare to shift out from register
-        shiftOut(SRDATAPIN, SRCLKPIN, MSBFIRST, pattern); // turn off lights
-        digitalWrite(SRLATCHPIN, HIGH);                   // latch data to output
-
-        lightsOutClock = millis();
-
-        #ifdef DEBUG
-        Serial.println("Lights out clock");
-        Serial.print(lightsOutClock);
-        #endif
-    }
-
-    //check for jump start
-
-    //ISR for buttons
-    if (buttonPressedP1 || buttonPressedP2) {
-        if (buttonPressedP1 && buttonPressedP2) {
-            if (reactionTimeP1 < reactionTimeP2) {
-                winner = 1; 
-                digitalWrite(P1LEDPIN, HIGH);
-            } else if (reactionTimeP2 < reactionTimeP1) {
-                winner = 2;
-                digitalWrite(P2LEDPIN,HIGH);
-            } else {
-                winner = 3;                                 //tie and light both warning leds
-                digitalWrite(P1JUMPLEDPIN, HIGH);
-                digitalWrite(P2JUMPLEDPIN, HIGH);
+    switch (currentState) {
+        case LINEUP:
+            if (digitalRead(P1BUTTONPIN) == LOW && digitalRead(P2BUTTONPIN) == LOW)         //game starts when both players press their buttons
+            {
+                delay(500);                                                                 // debounce delay
+                currentState = STARTSEQUENCE;
             }
-        } else if (buttonPressedP1) {
-            winner = 1;
-            digitalWrite(P1LEDPIN, HIGH);
-            digitalWrite(P2LEDPIN, LOW);
-        } else if (buttonPressedP2) {
-            winner = 2;
-            digitalWrite(P2LEDPIN, HIGH);
+            break;
+        
+        case STARTSEQUENCE:
+            lightsOutDelay = random(200, 3000);
+            // game loop
+            delay(STARTSEQUENCEDELAY); 
+            
+            lightpattern();
+            delay(lightsOutDelay);
+            // LIGHTS OUT!
+            pattern = 0;
+            digitalWrite(SRLATCHPIN, LOW);                    // prepare to shift out from register
+            shiftOut(SRDATAPIN, SRCLKPIN, MSBFIRST, pattern); // turn off lights
+            digitalWrite(SRLATCHPIN, HIGH);                   // latch data to output
+            
+            lightsOutClock = millis();
+            
+            currentState = LIGHTSOUT;
+            #ifdef DEBUG
+            Serial.println("Lights out now!");
+            #endif
+            break;
+        
+        case LIGHTSOUT:
+            // Check for button presses or jump starts
+            if (jumpStartP1 || jumpStartP2) {
+                // Jump start detected
+                if (jumpStartP1 && !jumpStartP2) {
+                    winner = 2; // Player 2 wins if P1 jumped
+                    digitalWrite(P2LEDPIN, HIGH); 
+                } else if (jumpStartP2 && !jumpStartP1) {
+                    winner = 1; // Player 1 wins if P2 jumped
+                    digitalWrite(P1LEDPIN, HIGH);
+                } else {
+                    winner = 3; // Tie if both jumped
+                    digitalWrite(P1JUMPLEDPIN, HIGH);
+                    digitalWrite(P2JUMPLEDPIN, HIGH);
+                }
+                currentState = GAMEOVER;
+            }
+            else if (buttonPressedP1 || buttonPressedP2) {
+                // Normal game
+                if (buttonPressedP1 && buttonPressedP2) {
+                    if (reactionTimeP1 < reactionTimeP2) {
+                        winner = 1;
+                        digitalWrite(P1LEDPIN, HIGH);
+                    } else if (reactionTimeP2 < reactionTimeP1) {
+                        winner = 2;
+                        digitalWrite(P2LEDPIN, HIGH); 
+                    } else {
+                        winner = 3; // tie
+                        digitalWrite(P1JUMPLEDPIN, HIGH);
+                        digitalWrite(P2JUMPLEDPIN, HIGH);
+                    }
+                } else if (buttonPressedP1) {
+                    winner = 1;
+                    digitalWrite(P1LEDPIN, HIGH);
+                } else if (buttonPressedP2) {
+                    winner = 2;
+                    digitalWrite(P2LEDPIN, HIGH);
+                }
+                currentState = GAMEOVER;
+            }
+            break;
+        case GAMEOVER:
+            // Display results and reset
+            if (winner == 1) {
+            #ifdef DEBUG
+                Serial.print("Player 1 wins! Reaction time: ");
+                Serial.print(reactionTimeP1);
+                Serial.println(" ms");
+            #endif
+            } else if (winner == 2) {
+            #ifdef DEBUG
+                Serial.print("Player 2 wins! Reaction time: ");
+                Serial.print(reactionTimeP2);
+                Serial.println(" ms");
+            #endif
+            } else if (winner == 3) {
+            #ifdef DEBUG
+                Serial.println("Tie!");
+            #endif
+            }
+
+            // Reset for next game
+            winner = 0;
             digitalWrite(P1LEDPIN, LOW);
+            digitalWrite(P2LEDPIN, LOW);
+            digitalWrite(P1JUMPLEDPIN, LOW);
+            digitalWrite(P2JUMPLEDPIN, LOW);
+            delay(3000); // Short delay before next game
+            currentState = LINEUP;
+            break;
         }
-    }
-
-    //output times
-    #ifdef DEBUG
-        if (winner == 1) {
-            Serial.println("Player 1 wins! Reaction time: ");
-            Serial.print(reactionTimeP1);
-            Serial.print(" ms");
-        } else if (winner == 2) {
-            Serial.println("Player 2 wins! Reaction time: ");
-            Serial.print(reactionTimeP2);
-            Serial.print(" ms");
-        } else if (winner = 3) {
-            Serial.println("Tie !");
-        } else {
-            Serial.println("undertermined");
-        }
-    #endif
-
-    delay(5000); /// delay for a new game
-    gameRunning = false;
 }
 
 //put function definitions here:
@@ -145,7 +172,6 @@ void lightpattern() {
       pattern = pattern | (1 << i); // lights up each light in sequence by ORing each bit with the previous one shifted per second
   #ifdef DEBUG
       Serial.println(pattern, BIN);
-      Serial.println("");
   #endif
       digitalWrite(SRLATCHPIN, LOW); // prepare to shift out from register
       shiftOut(SRDATAPIN, SRCLKPIN, MSBFIRST, pattern);
@@ -157,19 +183,21 @@ void lightpattern() {
 /*****
 Purpose: ISR for p1 button
 *****/
-void p1ButtonISR(){
-    if (gameRunning && !buttonPressedP1) {
+void p1ButtonISR()
+{
+    if (currentState == LINEUP) {
+        jumpStartP1 = true;
+    }
+    else if (currentState == LIGHTSOUT && !buttonPressedP1) {         //game is running and handle debounce/holding buttons to prevent multiple 
         reactionTimeP1 = millis() - lightsOutClock;
         buttonPressedP1 = true;
     }
 }
-/*****
-Purpose: ISR for p2 button
-*****/
-void p2ButtonISR()
-{
-    if (gameRunning && !buttonPressedP2)
-    {
+
+void p2ButtonISR() {
+    if (currentState == LINEUP) {
+        jumpStartP2 = true;
+    } else if (currentState == LIGHTSOUT && !buttonPressedP2) {       //game is running and handle debounce/holding buttons to prevent multiple 
         reactionTimeP2 = millis() - lightsOutClock;
         buttonPressedP2 = true;
     }
